ae642d1046a78c062369526130ce8508f60c85a9
echottzhu(朱婷婷) <echottzhu@tencent.com>
2016-10-19 14:43:30 +0800

    refator 小相册完善图片删除功能

--- pages/firstPage/firstPage.js
+++ pages/firstPage/firstPage.js
@@ -2,7 +2,8 @@ Page({
   data: {
     imgList: ['http://dayu.oa.com/avatars/echottzhu/profile.jpg?1437197657'],
     previewIndex:0,
-    previewMode: false
+    previewMode: false,
+    modalHidden: true
   },
   uploadImg: function(){
     var that = this;
@@ -12,9 +13,10 @@ Page({
       success: function (res) {
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
         var tempFilePaths = res.tempFilePaths;
-        var list = [];
+        var list = that.data.imgList;
+        var deLength = list.length;
         for(var i = 0;i<tempFilePaths.length;i++){
-          list[i] = tempFilePaths[i];
+          list[deLength+i] = tempFilePaths[i];
         }
         //上传选择的照片
         that.setData({
@@ -28,13 +30,38 @@ Page({
     var previewIndex = this.data.imgList.indexOf(imgsrc);
     this.setData({
       previewIndex:previewIndex,
-      previewMode:true
+      previewMode:true,
+      actionSheetHidden: false
     })
   },
   leavePreviewImg: function(){
     this.setData({
       previewIndex: 0,
       previewMode:false
+    })
+  },
+  getCurrentImg:function(){
+    var currentIndex = this.data.previewIndex;
+    this.setData({
+      previewIndex: currentIndex
+    })
+  },
+  deleteImg:function(event){
+    this.setData({
+      modalHidden: false
+    })
+  },
+  handlerDelete: function(event){
+    this.data.imgList.splice(this.data.previewIndex,1);
+    var list = this.data.imgList;
+    this.setData({
+      imgList:list,
+      modalHidden: true
+    })
+  },
+  cancleDelete: function(){
+    this.setData({
+      modalHidden: true
     })
   },
   onLoad: function () {
